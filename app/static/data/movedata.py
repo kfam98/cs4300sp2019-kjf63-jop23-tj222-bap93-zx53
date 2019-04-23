84fbdfb9e9e7e1f91102d75b00e1c2ebcc0427ca
Kelly Fam <kellyfam@dhcp-gs-1238.eduroam.cornell.edu>
2019-04-22 21:30:35 -0400

    added missing image links on results page

--- app/static/data/movedata.py
+++ app/static/data/movedata.py
@@ -19,20 +19,17 @@ pokemon_lst = {}
 #     data = json.load(json_file)
 #     old_pkmn = data
 
+image_lst = {}
+
 
 with open('pokemondata2.json') as json_file:
     data = json.load(json_file)
     for pokemon in data:
-      if "type1" in pokemon.keys() and "type2" in pokemon.keys():
-          type1 = pokemon['type1']
-          type2 = pokemon['type2']
-          if type2 == None:
-            type2 = ''
-          pokemon_lst[pokemon['name']] = {'type1': type1, 'type2':type2}
+      image_lst[pokemon['name']] = pokemon['imgSrc']
 
 
-with open('typeslst.json', 'w') as outfile:
-    json.dump(pokemon_lst, outfile)
+with open('imageslst.json', 'w') as outfile:
+    json.dump(image_lst, outfile)
 
 # with open('pokemondata2.json', 'w') as outfile:
 #     json.dump(new_pkmon, outfile)
