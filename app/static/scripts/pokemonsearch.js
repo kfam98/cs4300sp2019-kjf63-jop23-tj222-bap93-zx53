b0b481010b0722a4d98c345b0f5c0a9c033e0699
Kelly Fam <kellyfam@dhcp-gs-1238.eduroam.cornell.edu>
2019-04-22 21:55:59 -0400

    attempt to fix

--- app/static/scripts/pokemonsearch.js
+++ app/static/scripts/pokemonsearch.js
@@ -15,7 +15,7 @@ var selectedGenerations = ['1','2','3','4','5','6','7'];
 var selectedLegendary = 0;
 
 
-$.getJSON("/static/data/pokemondata2.json", function(json) {
+$.when($.getJSON("/static/data/pokemondata2.json")).then( function(json) {
   for (var i = 0; i < json.length; i++) {
     dataList.push({id: i, text: json[i].name, generation: json[i].generation, legendary: json[i].legendary, pokedex_number: json[i].pokedex_number});
   }
